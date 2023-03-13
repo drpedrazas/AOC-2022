ebbb0999192f7814ca0cf0ee62a910191bf478c7
david <drpedrazas@unal.edu.co>
2023-03-13 14:51:10 -0500

    day 6 parts 1 and 2

---
+++ haskell/day_6/day_6.hs
@@ -0,0 +1,10 @@
+import Data.List
+unique ls | xs  == [] = True
+          | x == head xs =False
+          | otherwise =unique xs
+        where (x:xs) = sort ls
+
+solution ls = prm treated
+                where prm (x: xs) = if fst x then (snd x) + 3 else prm xs
+                      treated = zip (mapped ls) [1..]
+                            where mapped y = [unique (take 4 y)] ++ mapped (tail y)        
